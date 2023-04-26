937e2f33da7baa1b81590d37df885ef6d655818d
Marks Ruziboev <mruziboev@gacrux.mat.univie.ac.at>
2023-04-26 16:26:18 +0200

    from office

--- fractol-final/create.c
+++ fractol-final/create.c
@@ -1,12 +1,43 @@
 
 #include "fractol.h"
 
-void    img_wind(t_mlx *w, char *str) // type mlx
+void    init(t_mlx *w)
 {
+	w->mlx = NULL;
+	w->win = NULL;
+	w->img = NULL;
+	w->addr = NULL;
+    w->color = NULL;
+	w->bits_per_pixel = 0;
+	w->line_length = 0;
+	w->endian = 0;
+	w->x = 0;
+	w->y = 0;
+	w->x_max = 0;
+	w->y_max = 0;
+	w->x_min = 0;
+	w->y_min = 0;
+	w->cx = 0;
+	w->cy = 0;
+}
+
+void    img_wind(t_mlx *w, char *str) // type mlx
+{     
+    w->color = ft_calloc(OMEGA + 1, sizeof(int));
+    if(!(w->color))
+        exit_husseyin(1, w);
     w->mlx = mlx_init();
+    if (!w->mlx)
+        exit_husseyin(1, w);
 	w->win = mlx_new_window(w->mlx, HEIGHT, HEIGHT, str);
+    if (!w->win)
+        exit_husseyin(1, w);
 	w->img = mlx_new_image(w->mlx, HEIGHT, HEIGHT);
+    if (!w->img)
+        exit_husseyin(1, w);
     w->addr = mlx_get_data_addr(w->img, &(w->bits_per_pixel), &(w->line_length), &(w->endian));
+    if (!w->addr)
+        exit_husseyin(1, w);
 }
 
 void    my_mlx_pixel_put(t_mlx *w, t_ivec *p, int color)
